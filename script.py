ee7cd0d9557d68c7e4349ea0b7bf7eb32d8149ae
Cyril <cyril.novel@gmail.com>
2014-03-23 20:55:50 +0000

    Create the script file and the Phone nb processing function

---
+++ script.py
@@ -0,0 +1,89 @@
+################################
+# Work under CC BY 4.0 license #
+#      by Cyril NOVEL          #
+################################
+
+# Process the phone number
+def numberProcessing(stringNb):
+  newNumber
+  if stringNb[0] == "+":
+    space = False
+    for(c in stringNb):
+      if c == " ":
+        space = True
+
+    if !space:
+      if (stringNb[1] == 3 and stringNb[2] == 3):
+        # french number
+        newNumber = "+33 "
+        newNumber += stringNb[3]
+        i = 4
+        while i < len(stringNb):
+          if i%2 == 0:
+            newNumber += " " + stringNb[i]
+          else:
+            newNumber += stringNb[i]
+          i += 1
+
+        return newNumber
+
+      elif (stringNb[1] == 4 and stringNb[2] == 4):
+        # UK number
+        newNumber = "+44 "
+        newNumber += stringNb[3]
+        i = 4
+        while i < len(stringNb):
+          if i%7 == 0:
+            newNumber += " " + stringNb[i]
+          else:
+            newNumber += stringNb[i]
+          i += 1
+
+        return newNumber
+
+      else:
+        return stringNb
+
+    else:
+      return stringNb
+
+  else:
+    space = False
+    for(c in stringNb):
+      if c == " ":
+        space = True
+
+    if space:
+      return stringNb
+    else:
+      if len(stringNb) == 10:
+        # french number
+        newNumber = "+33 "
+        newNumber += stringNb[1]
+        i = 2
+        while i < len(stringNb):
+          if i%2 == 0:
+            newNumber += " " + stringNb[i]
+          else:
+            newNumber += stringNb[i]
+          i += 1
+
+        return newNumber
+
+      elif len(stringNb) == 11:
+        # UK number
+        newNumber = "+44 "
+        newNumber += stringNb[1]
+        i = 2
+        while i < len(stringNb):
+          if i%5 == 0:
+            newNumber += " " + stringNb[i]
+          else:
+            newNumber += stringNb[i]
+          i += 1
+
+        return newNumber
+
+      else:
+        return stringNb
+
